0bd538305684fc7643fc04ef78aac15308b08b79
Nimit Haria <nimitharia007@gmail.com>
2024-07-15 00:44:48 +0530

    feat: added limbo game

--- backend/src/features/games/games.router.ts
+++ backend/src/features/games/games.router.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import plinkooRouter from './plinkoo/plinkoo.router';
 import minesRouter from './mines/mines.router';
+import limboRouter from './limbo/limbo.router';
 
 const gameRouter = Router();
 
 gameRouter.use('/plinkoo', plinkooRouter);
 gameRouter.use('/mines', minesRouter);
+gameRouter.use('/limbo', limboRouter);
 
 export default gameRouter;
